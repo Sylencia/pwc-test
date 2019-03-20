345e49c86d2c9a7b605605936c10b9ddfeb2d453
Joseph <valrigardian@gmail.com>
2019-03-20 23:54:56 +1100

    Add filtered todo test

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,4 +1,42 @@
-import { getPriorityAsString, getTotalTodos, getCompletedTodos } from '.'
+import {
+  getPriorityAsString,
+  getTotalTodos,
+  getCompletedTodos,
+  getFilteredTodos,
+} from '.'
+
+const todos = [
+  {
+    id: '1',
+    text: 'bbbbbb',
+    priority: 1,
+    isCompleted: false,
+  },
+  {
+    id: '2',
+    text: 'aaaaaa',
+    priority: 2,
+    isCompleted: true,
+  },
+  {
+    id: '3',
+    text: 'dddddd',
+    priority: 3,
+    isCompleted: false,
+  },
+  {
+    id: '4',
+    text: 'eeeeee',
+    priority: 1,
+    isCompleted: true,
+  },
+  {
+    id: '5',
+    text: 'cccccc',
+    priority: 2,
+    isCompleted: false,
+  },
+]
 
 describe('getPriorityAsString', () => {
   it('returns the correct string', () => {
@@ -10,39 +48,6 @@ describe('getPriorityAsString', () => {
 })
 
 describe('Todo Counts', () => {
-  const todos = [
-    {
-      id: '1',
-      text: 'test',
-      priority: 1,
-      isCompleted: false,
-    },
-    {
-      id: '2',
-      text: 'test',
-      priority: 2,
-      isCompleted: true,
-    },
-    {
-      id: '3',
-      text: 'test',
-      priority: 3,
-      isCompleted: false,
-    },
-    {
-      id: '4',
-      text: 'test',
-      priority: 1,
-      isCompleted: true,
-    },
-    {
-      id: '5',
-      text: 'test',
-      priority: 2,
-      isCompleted: false,
-    },
-  ]
-
   it('returns the correct number of total todos', () => {
     expect(getTotalTodos([])).toEqual(0)
     expect(getTotalTodos(todos)).toEqual(5)
@@ -51,5 +56,100 @@ describe('Todo Counts', () => {
   it('returns the correct number of completed todos', () => {
     expect(getCompletedTodos([])).toEqual(0)
     expect(getCompletedTodos(todos)).toEqual(2)
+  })
+})
+
+describe('Sorting and Filtering Todos', () => {
+  it('returns the correct list when sorting by name', () => {
+    expect(getFilteredTodos(todos, 'name', true)).toEqual([
+      {
+        id: '2',
+        text: 'aaaaaa',
+        priority: 2,
+        isCompleted: true,
+      },
+      {
+        id: '1',
+        text: 'bbbbbb',
+        priority: 1,
+        isCompleted: false,
+      },
+      {
+        id: '5',
+        text: 'cccccc',
+        priority: 2,
+        isCompleted: false,
+      },
+      {
+        id: '3',
+        text: 'dddddd',
+        priority: 3,
+        isCompleted: false,
+      },
+      {
+        id: '4',
+        text: 'eeeeee',
+        priority: 1,
+        isCompleted: true,
+      },
+    ])
+  })
+
+  it('returns the correct list when sorting by priority', () => {
+    expect(getFilteredTodos(todos, 'priority', true)).toEqual([
+      {
+        id: '1',
+        text: 'bbbbbb',
+        priority: 1,
+        isCompleted: false,
+      },
+      {
+        id: '4',
+        text: 'eeeeee',
+        priority: 1,
+        isCompleted: true,
+      },
+      {
+        id: '2',
+        text: 'aaaaaa',
+        priority: 2,
+        isCompleted: true,
+      },
+      {
+        id: '5',
+        text: 'cccccc',
+        priority: 2,
+        isCompleted: false,
+      },
+      {
+        id: '3',
+        text: 'dddddd',
+        priority: 3,
+        isCompleted: false,
+      },
+    ])
+  })
+
+  it('returns the correct list when filtering', () => {
+    expect(getFilteredTodos(todos, 'name', false)).toEqual([
+      {
+        id: '1',
+        text: 'bbbbbb',
+        priority: 1,
+        isCompleted: false,
+      },
+      {
+        id: '5',
+        text: 'cccccc',
+        priority: 2,
+        isCompleted: false,
+      },
+      {
+        id: '3',
+        text: 'dddddd',
+        priority: 3,
+        isCompleted: false,
+      },
+    ])
   })
 })
